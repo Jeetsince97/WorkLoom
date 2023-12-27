7a010057c7be263c1ae45663940375d139a8753c
Simranjeet Singh <simranjeets224@gmail.com>
2023-12-27 14:41:46 -0800

    chore: setup logo and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,19 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { siteConfig } from "@/config/site";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "WorkLoom",
-  description: "Manage your team's work in one place.",
+  title: { default: siteConfig.name, template: `%s | ${siteConfig.name}` },
+  description: siteConfig.description,
+  icons: [
+    {
+      url: "/logo.svg",
+      href: "/logo.svg",
+    },
+  ],
 };
 
 export default function RootLayout({
---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "WorkLoom",
+  description:
+    "WorkLoom is a platform for managing your work and your team's work.",
+};
